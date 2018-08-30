efa30c1b61c05b3e9423bd412d0e2e7189c33079
Emric <emric.mansson@gmail.com>
2018-08-30 10:43:32 +0200

    Move spec configuration to its own file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "bundler/setup"
 require "cardonline"
 require "webmock/rspec"
 
+Dir["spec/support/**/*.rb"].each { |file| require_relative "../#{file}" }
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
---
+++ spec/support/cardonline.rb
@@ -0,0 +1,5 @@
+Cardonline.configure do |config|
+  config.base_url = 'https://cardonline.se/test_api'
+  config.username = 'username'
+  config.username = 'password'
+end
